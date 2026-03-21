ba2af5e65036a5e341375172db0b1b7832b4aa55
MabMab^^ <miguelberrioh@hotmail.com>
2026-03-20 21:08:54 -0500

    Clase 2

---
+++ Semana1/Semana1/Controllers/NegociosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Semana1.Controllers
+{
+    public class NegociosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
