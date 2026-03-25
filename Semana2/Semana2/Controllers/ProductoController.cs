156fe3a74298965f2835f402ce9e811d2679bc9c
MabMab^^ <miguelberrioh@hotmail.com>
2026-03-24 22:34:52 -0500

    Clase 3

---
+++ Semana2/Semana2/Controllers/ProductoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Semana2.Models;
+
+namespace Semana2.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        IEnumerable<Producto> getAllProductos()
+        {
+            List<Producto> productos = new List<Producto>();
+            using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:database"]))
+            {
+                connection.Open();
+                string query = "SELECT id, descripcion, marca, precio, stock FROM Producto";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Producto producto = new Producto
+                            {
+                                id = reader.GetInt32(0),
+                                descripcion = reader.GetString(1),
+                                marca = reader.GetString(2),
+                                precio = reader.GetDecimal(3),
+                                stock = reader.GetInt32(4)
+                            };
+                            productos.Add(producto);
+                        }
+                    }
+                }
+            }
+            return productos;
+        }
+
+        public IActionResult Index()
+        {
+            return View(getAllProductos());
+        }
+    }
+}
---
+++ Semana2/Semana2/Models/Producto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Semana2.Models
+{
+    public class Producto
+    {
+        [Display(Name = "Id Producto")] public int id { get; set; }
+        [Display(Name = "Descripcion")] public string descripcion { get; set; }
+        [Display(Name = "Marca")] public string marca { get; set; }
+        [Display(Name = "Precio c/u")] public decimal precio { get; set; }
+        [Display(Name = "Cantidad")] public int stock { get; set; }
+    }
+}
