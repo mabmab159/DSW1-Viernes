d98a62a859bc32dcb1184781417f01b607fdcc57
MabMab^^ <miguelberrioh@hotmail.com>
2026-04-21 22:42:03 -0500

    Clase 11

---
+++ semana6/semana6/wwwroot/js/Chat.js
@@ -0,0 +1,23 @@
+﻿var cn = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+cn.on("ReceiveMessage", function (user, message) {
+    let fecha = new Date().toLocaleDateString();
+    let mensaje = "<div>" + fecha + ";" + user + ":" + message + "</div>";
+    document.getElementById("messagesList").innerHTML += mensaje;
+});
+
+cn.start().then(function () {
+    document.getElementById("messagesList").innerHTML = "";
+}).catch(function (err) {
+    return console.error(err.toString());
+})
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    let usuario = document.getElementById("userInput").value;
+    let mensaje = document.getElementById("messageInput").value;
+
+    cn.invoke("SendMessage", usuario, mensaje).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
